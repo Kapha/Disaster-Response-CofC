deb37f903ad3f38ff10836d6dfa18e845caafc8a
Peter Blomgren <blomgren-p@mssu.edu>
2013-05-28 16:06:02 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DisasterReliefCoc::Application.config.secret_token = '8859b83f19cfdb3a3ca2f1982662b8ea44583d8b5e650189d24891238515d298c682122aa83c3d998a4150a2807777584c92d51243617d7fde0b737f56ad9f75'
